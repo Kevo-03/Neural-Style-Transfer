9262b3ca451eac51bec34f966d69bf679445735d
Kıvanç Onat Türker <kivo0326@gmail.com>
2026-02-25 00:18:52 +0300

    added schemas and security files

---
+++ backend/app/security.py
@@ -0,0 +1,10 @@
+from pwdlib import PasswordHash
+
+# Automatically selects Argon2 and configures the best security settings
+password_hash = PasswordHash.recommended()
+
+def get_password_hash(password: str) -> str:
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return password_hash.verify(plain_password, hashed_password)
