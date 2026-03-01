10d25e9e81270b96fd3326c372a2fc5455c6868b
Kıvanç Onat Türker <kivo0326@gmail.com>
2026-03-01 04:12:22 +0300

    update auth.py to previous local version

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -64,7 +64,6 @@ def login(
         httponly=True,   # JavaScript cannot read this (highly secure)
         secure=True,    # Set to True in production (HTTPS)
         samesite="lax",  # Protects against CSRF attacks
-        domain=".neuralart.app",
         max_age=settings.access_token_expire_minutes * 60 # 30 minutes (match your token expiration)
     )
 
