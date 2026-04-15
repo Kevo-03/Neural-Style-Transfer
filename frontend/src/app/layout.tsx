a6b22d10c12f42860e29a4260b955ee87b447754
Kıvanç Onat Türker <kivo0326@gmail.com>
2026-04-15 23:06:20 +0300

    refactor: image preloads in layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,8 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preload" href="/demo-output.jpg" as="image" />
-        <link rel="preload" href="/signup_photo.jpg" as="image" />
+        <link rel="preload" href="/demo-content.jpg" as="image" fetchPriority="high" />
+        <link rel="preload" href="/demo-style.jpg" as="image" fetchPriority="high" />
+        <link rel="preload" href="/demo-output.jpg" as="image" fetchPriority="high" />
+        <link rel="preload" href="/signup_photo.jpg" as="image" fetchPriority="high" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900 text-white min-h-screen`}
